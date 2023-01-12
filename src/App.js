fe0e9889b12e2c6cf85755fc068d899a17b76ca4
Aman <aman.vohra@asambhav.org.in>
2023-01-12 11:08:00 +0530

    Managed Commit 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
+import Appp from './DND/components/Appp';
+import About from './DND/components/About';
 
 function App() {
+  const isLoggedIn = window.localStorage.getItem("loggedIn")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={isLoggedIn == "true" ? <Appp /> : <Login />}></Route>
+          <Route path="/Signup" element={<Signup/>}></Route>
+          <Route path="/Managed" element={<Appp/>}></Route>
+          <Route path="/About" element={<About/>}></Route>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "../src/DND/store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  
   <React.StrictMode>
+    <Provider store={store}>
+    <PersistGate persistor={persistor}>
     <App />
+    </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
 
